418fc3debcba55aed29698dea5b10011e575d971
collins-ruto <collinsruto48@gmail.com>
2022-05-18 23:01:32 +0300

    Working App

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
-
     println!("Hello and welcome to my guessing game!");
-    println!("Please enter your guess below: ");
 
-    let mut guess = String::new();
+    let random_num = rand::thread_rng().gen_range(1..101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Something went wrong");
+    loop{
+        println!("Please enter your guess below: ");
 
-    println!("you guesses : {}", guess);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Something went wrong");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed : {}", guess);
+
+        match guess.cmp(&random_num) {
+            Ordering::Less => println!("Guess higher"),
+            Ordering::Greater => println!("Guess lower"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
+        }
+    }
 }
